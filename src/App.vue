290d4c507346262f51719441e9d4d19e23c05e95
Rajat <rajatdoijad67@gmail.com>
2022-07-19 17:58:28 +0530

    hello

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+   <HelloWorld msg="Welcome to Your Vue.js App"/> 
+  
+    
+    <home/>
+
+</template>
+
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    home,
+  }
+}
+</script>
+
+
+
+
---
+++ src/components/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1> Home Component {{"anil".length}}</h1>
+  <button v-on:click="getName('Button 1 clicked')">Click Me</button>
+   <button v-on:mousemove="getName('Button 1 clicked')">Click Me</button>
+  
+  
+  <h1>Name {{getName("Naruto")}}</h1>
+</template>
+<script>
+export default {
+      name:'homeComponent',
+      data(){
+        return{
+            email:'anil@joke.com',
+            mobile:234567890,
+            getName:function(){
+              alert("Hello ");
+            }
+        }
+      }
+}
+</script>
+<style scoped>
+h1{
+    color: orangered;
+}
+</style>
