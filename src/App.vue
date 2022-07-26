27fe64969181d81f5fe2eafd01058ff7ce15c8a1
Rajat <rajatdoijad67@gmail.com>
2022-07-26 17:35:00 +0530

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <!--<img alt="Vue logo" src="./assets/logo.png">-->
    <HelloWorld msg="Welcome to Your Vue.js App"/> 
   
     
-    <home/>
+    <home  msg="hii from anil"/>
 
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,42 +1,25 @@
 <template>
-   <h1>Get the input field value</h1>
-   <h3>Technology</h3>
-    <label for="java">java</label>
-    <input type="checkbox" value="java" v-model="technology" id="java">
-
-    <label for="php">php</label>
-    <input type="checkbox" value="php" v-model="technology" id="php">
-    
-    <label for="node">node</label>
-    <input type="checkbox" value="node" v-model="technology" id="node">
-     <br/><br/>
-     <h3>who am i</h3>
-     <label for="student">Student</label>
-    <input type="radio" value="student"  v-model="who" name="who" id="student">
-     
-      <label for="developer">Developer</label>
-    <input type="radio" value="developer" v-model="who" name="who"  id="developer">
-     
-     <br>
-      
-    <h3>Selected Technology1: {{technology}}</h3>
-  
-  
-  
+ 
+ <h1>Hello from home componenet</h1>
+ <h2>{{data}}</h2>
+ <h2>{{msg}}</h2>
+ 
   
 </template>
 <script>
 export default {
       name:'homeComponent',
-      data()
-      {
-        return {
-            technology: [],
-            who: null
-        }
+      props:{
+        data: String,
+        msg: String
+
       }
+
+
    
       }
+   
+      
     
    
 
